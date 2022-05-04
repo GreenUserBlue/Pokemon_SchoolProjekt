5c38efdcdb59bb539d7926b3e0e0c05b13ef4790
Clemens Hodina <9183@htl.rennweg.at>
2022-05-04 09:45:34 +0200

    Pokemon sehr viel weiter --> Stats passen mot level überein etc

--- src/InGame/Pokemon.java
+++ src/InGame/Pokemon.java
@@ -7,8 +7,16 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Pokemon {
+
+    //TODO Type
+    //TODO Attacken
+    //TODO entwicklungen
+    //TODO Bilder
+
+
 
     private String name;
 
@@ -37,15 +45,26 @@ public class Pokemon {
 
     private String growthRate;
 
+    private State state;
+
+    //individual values für stats
+    private int[] iv;
+
     public static List<Pokemon> template = new ArrayList<>();
 
-
+        private static final Random rnd=new Random(696969);
 
     public static void main(String[] args) {
         Pokemon.init();
+        /*for (Pokemon pokemon : template) {
+            System.out.println(pokemon);
+        }
 
-        Pokemon a = createPokemon(new Vector2D(900,900), World.Block.Water);
+         */
+
+        Pokemon a = createPokemon(new Vector2D(3000,2471), World.Block.Grass);
         System.out.println(a);
+
     }
 
 
@@ -54,24 +73,32 @@ public class Pokemon {
     //und gibts in die tatsächliche Liste
     public static void init() {
         //File file = new File(String.valueOf(Path.of("res/DataSets/pokeFile.txt")));
-        BufferedReader in = null;
+        BufferedReader in;
+        BufferedReader in2;
         String[] lines = new String[151];
+        String[] lines2 = new String[151];
+        State s;
         try {
             in = new BufferedReader(new FileReader("res/DataSets/pokeFile.txt"));
-            String row = null;
+            in2 = new BufferedReader(new FileReader("res/DataSets/dataList.txt"));
+            String row;
+            String row2;
             for (int i = 0; (row = in.readLine()) != null; i++) {
                 lines[i] = row;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            for (int i = 0; (row2 = in2.readLine()) != null && i < 151; i++) {
+                lines2[i] = row2;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String[] oneRow = new String[6];//bis jz nur 6 attribute
-        int maxXp = 0;
+        String[] oneRow;//bis jz nur 6 attribute
+        String[] oneRow2;
+        int maxXp;
         World.Block block;
         for (int i = 0; i < 151; i++) {
             oneRow = lines[i].split(";");
+            oneRow2 = lines2[i].split(",");
             maxXp = getXpNeeded(oneRow[5], 1);
             if (oneRow[4].contains("water") || oneRow[4].contains("sea")) {
                 block = World.Block.Water;
@@ -80,36 +107,35 @@ public class Pokemon {
             } else {
                 block = World.Block.Grass;
             }
-            template.add(new Pokemon(oneRow[1], Integer.parseInt(oneRow[0]), null, null, null, null, 1, 0, maxXp, Integer.parseInt(oneRow[2]), block, oneRow[5]));
+            s = new State(Integer.parseInt(oneRow2[5]), Integer.parseInt(oneRow2[6]), Integer.parseInt(oneRow2[7]), Integer.parseInt(oneRow2[8]), Integer.parseInt(oneRow2[9]), Integer.parseInt(oneRow2[10]));
+            template.add(new Pokemon(oneRow[1], Integer.parseInt(oneRow[0]), null, null, null, null, 1, 0, maxXp, Integer.parseInt(oneRow[2]), block, oneRow[5], s, new int[6]));
         }
 
 
     }
 
     /**
-     * @param levelType
-     * @param curLevel  das is das tatsächlich derzeitige Level
-     * @return
+     * @param levelType how fast a pokemons gains xp
+     * @param curLevel the current level
+     * @return how much exp needed for next level
      */
     private static int getXpNeeded(String levelType, int curLevel) {
-        int maxXp = 0;
+        int maxXp;
         curLevel++;
-        if (levelType.equals("fast")) {
-            maxXp = (int) ((4 * Math.pow(curLevel, 3)) / 5);
-        } else if (levelType.equals("medium")) {
-            maxXp = (int) Math.pow(curLevel, 3);
-        } else if (levelType.equals("medium-slow")) {
-            maxXp = (int) (((6 * Math.pow(curLevel, 3)) / 5) - (15 * Math.pow(curLevel, 2)) + (100 * curLevel) - 140);
-        } else {//if (levelType.equals("slow"))
-            maxXp = (int) ((5 * Math.pow(curLevel, 3)) / 4);
-        }
+        maxXp = switch (levelType) {// wenn hier was nd geht is der switch schuld
+            case "fast" -> (int) ((4 * Math.pow(curLevel, 3)) / 5);
+            case "medium" -> (int) Math.pow(curLevel, 3);
+            case "medium-slow" -> (int) (((6 * Math.pow(curLevel, 3)) / 5) - (15 * Math.pow(curLevel, 2)) + (100 * curLevel) - 140);
+            default -> //if (levelType.equals("slow"))
+                    (int) ((5 * Math.pow(curLevel, 3)) / 4);
+        };
         return maxXp;
     }
 
 
 
 
-    public Pokemon(String name, int id, EvolveType evolveType, Attack[] attacks, Nature nature, Type[] type, int level, int xp, int maxXP, int captureRate, World.Block block, String growthRate) {
+    public Pokemon(String name, int id, EvolveType evolveType, Attack[] attacks, Nature nature, Type[] type, int level, int xp, int maxXP, int captureRate, World.Block block, String growthRate, State state, int[] iv) {
         this.name = name;
         this.id = id;
         this.evolveType = evolveType;
@@ -122,32 +148,39 @@ public class Pokemon {
         this.captureRate = captureRate;
         this.block = block;
         this.growthRate = growthRate;
+        this.state = state;
+        this.iv = iv;
     }
 
 
-    //TODO neues Bild
 
 
-
-
-
-    //TODO die Stats an das Level anpassen
     private static Pokemon getPokemon(int id, int level) {
+        //da id mit 1 beginnt
+        id--;
         Pokemon a = new Pokemon();
         try {
-            a = template.get(id-1).clone();
+            a = template.get(id).clone();
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
         a.level = level;
-        a.xp = getXpNeeded(a.growthRate, level-1);//TODO is die xp wenn man gerade auf das level gekommen ist
+        a.xp = getXpNeeded(a.growthRate, level-1);//is die xp wenn man gerade auf das level gekommen ist
         a.maxXP=getXpNeeded(a.growthRate, level);
+        a.nature = Nature.values()[(int) ((Math.random()) * Nature.values().length)];
+        for (int i = 0; i < 6; i++) {
+            a.iv[i] = rnd.nextInt(16);
+        }
+        System.out.println(template.get(id).getState());
+        a.state.add(id, level, a.nature);
         return a;
     }
 
+    @SuppressWarnings("MethodDoesntCallSuperMethod")
     @Override
     protected Pokemon clone() throws CloneNotSupportedException {
-        return new Pokemon(name, id, evolveType, attacks, nature, type, level, xp, maxXP, captureRate, block, growthRate);
+        //Pokemon clone = (Pokemon) super.clone();
+        return new Pokemon(name, id, evolveType, attacks, nature, type, level, xp, maxXP, captureRate, block, growthRate, state, iv);
     }
 
     public Pokemon() {
@@ -162,7 +195,7 @@ public class Pokemon {
      * makes a Pokemon from the String which was sent from the server
      *
      * @param msg the message from the server
-     * @return
+     * @return pokemon from server
      */
     private static Pokemon getFromMsg(String msg) {
 
@@ -172,7 +205,7 @@ public class Pokemon {
     /**
      * makes a String to send from the server to the client
      *
-     * @return
+     * @return string to server
      */
     private String toMsg() {
         return "Hier ist ein Pokemon namens Ditto mit den Attacken {Tackle,AP4,MAXAP17}{Platcher,AP30,MAXAP30} und dem Wesen usw.";
@@ -189,11 +222,11 @@ public class Pokemon {
     //xpoverride is wie viele xp man ins nächste level mitnimmt
     private void levelUp(int xpOverride) {
         level++;
+        state.add(id, level, nature);
         xp = xpOverride;
     }
 
     //pos für wie weit vom spawnt entfernt
-    //TODO es is nicht beachtet wegen Grass und Wasser
     //TODO vl schöner machen und sachen in Methoden auslagern
     private static Pokemon createPokemon(Vector2D pos, World.Block block) {
         int distance = (int) pos.magnitude();
@@ -228,7 +261,7 @@ public class Pokemon {
                     }
                 }
             }
-            int number = (int) (Math.random() * (possibilities.size() - 1 + 1) + 1);
+            int number = (int) (Math.random() * (possibilities.size()));
             return getPokemon(possibilities.get(number).id, level);
         } else if (block.equals(World.Block.Water)) {
             if (distance >= 0 && distance <= 150) {
@@ -317,6 +350,8 @@ public class Pokemon {
         public double[] getEffect() {
             return values;
         }
+
+
     }
 
     @Override
@@ -333,6 +368,22 @@ public class Pokemon {
                 ", maxXP=" + maxXP +
                 ", captureRate=" + captureRate +
                 ", block=" + block +
+                ", growthRate='" + growthRate + '\'' +
+                ", state=" + state.toString() +
+                ", iv=" + Arrays.toString(iv) +
                 '}';
+    }
+
+
+    public State getState() {
+        return state;
+    }
+
+    public int[] getIv() {
+        return iv;
+    }
+
+    public Nature getNature() {
+        return nature;
     }
 }
